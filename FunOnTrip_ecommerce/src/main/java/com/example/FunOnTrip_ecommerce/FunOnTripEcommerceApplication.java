a7272490ccc8e756add95d7a015586afc8de1c86
Angélica Torres <anguietorres.92@gmail.com>
2026-01-07 16:45:17 -0600

    Remove unused folder <nombre_de_la_carpeta>

---
+++ FunOnTrip_ecommerce/src/main/java/com/example/FunOnTrip_ecommerce/FunOnTripEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.FunOnTrip_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunOnTripEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunOnTripEcommerceApplication.class, args);
+	}
+
+}
