7ea9f92291aea568542115d672c4bee0e84934e1
Angélica Torres <anguietorres.92@gmail.com>
2026-01-15 22:39:35 -0600

    Ya se realizaron las pruebas en postman y si jala

--- FunOnTrip_ecommerce/src/main/java/FunOnTrip/ecommerce/repository/ContactoRepository.java
+++
@@ -1,9 +0,0 @@
-package FunOnTrip.ecommerce.repository;
-
-import FunOnTrip.ecommerce.model.Contacto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ContactoRepository extends JpaRepository<Contacto, Integer> {
-    List<Contacto> findByAtendidoFalse();
-}
